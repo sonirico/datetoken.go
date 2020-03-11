2769869f042fc5a28b689aad65f37454b961e0a6
sonirico <marsanben92@gmail.com>
2020-03-11 21:59:35 +0100

    feat: add snap start tests

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -1,10 +1,15 @@
 package evaluator
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
+
+const testLayout = "2006-01-02 15:04:05"
+
+func formatTime(date time.Time) string {
+	return date.Format(testLayout)
+}
 
 func testEval(t *testing.T, payload string, initial time.Time) time.Time {
 	t.Helper()
@@ -18,42 +23,65 @@ func testEval(t *testing.T, payload string, initial time.Time) time.Time {
 	return time
 }
 
+func assertTime(t *testing.T, date time.Time, expected string) bool {
+	if formatTime(date) != expected {
+		t.Errorf("unexpected result. want '%s', have '%s'",
+			expected, formatTime(date))
+		return false
+	}
+	return true
+}
+
 func parseTime(date string) time.Time {
-	res, _ := time.Parse("2006-01-02 15:04:05", date)
-	fmt.Println(fmt.Sprintf("time %v(%T)", res, res))
+	res, _ := time.Parse(testLayout, date)
 	return res
 }
 
 func TestEvaluator_SnapStartSecond(t *testing.T) {
 	time := testEval(t, "now/s", parseTime("2020-01-01 10:13:23"))
-	if time.Nanosecond() != 0 {
-		t.Errorf("expected nanosecond to be '0', got '%d'", time.Second())
+	if !assertTime(t, time, "2020-01-01 10:13:23") {
+		t.FailNow()
 	}
 }
 
 func TestEvaluator_SnapStartMinute(t *testing.T) {
 	time := testEval(t, "now/m", parseTime("2020-01-01 10:13:23"))
-	if time.String() != "2020-01-01 10:13:23" {
-		t.Errorf("mira mi t %s", time.String())
+	if !assertTime(t, time, "2020-01-01 10:13:20") {
 		t.FailNow()
-	}
-	if time.Nanosecond() != 0 {
-		t.Errorf("expected nanosecond to be '0', got '%d'", time.Second())
-	}
-	if time.Second() != 0 {
-		t.Errorf("expected second to be '0', got '%d'", time.Second())
 	}
 }
 
 func TestEvaluator_SnapStartHour(t *testing.T) {
 	time := testEval(t, "now/h", parseTime("2020-01-01 10:13:23"))
-	if time.Nanosecond() != 0 {
-		t.Errorf("expected nanosecond to be '0', got '%d'", time.Second())
+	if !assertTime(t, time, "2020-01-01 10:00:00") {
+		t.FailNow()
 	}
-	if time.Second() != 0 {
-		t.Errorf("expected second to be '0', got '%d'", time.Second())
+}
+
+func TestEvaluator_SnapStartDay(t *testing.T) {
+	time := testEval(t, "now/d", parseTime("2020-01-01 10:13:23"))
+	if !assertTime(t, time, "2020-01-01 00:00:00") {
+		t.FailNow()
 	}
-	if time.Second() != 0 {
-		t.Errorf("expected minute to be '0', got '%d'", time.Second())
+}
+
+func TestEvaluator_SnapStartWeek(t *testing.T) {
+	time := testEval(t, "now/w", parseTime("2020-01-01 10:13:23"))
+	if !assertTime(t, time, "2020-01-01 10:00:00") {
+		t.FailNow()
+	}
+}
+
+func TestEvaluator_SnapStartMonth(t *testing.T) {
+	time := testEval(t, "now/M", parseTime("2020-01-01 10:13:23"))
+	if !assertTime(t, time, "2020-01-01 10:00:00") {
+		t.FailNow()
+	}
+}
+
+func TestEvaluator_SnapStartYear(t *testing.T) {
+	time := testEval(t, "now/Y", parseTime("2020-03-01 10:13:23"))
+	if !assertTime(t, time, "2020-01-01 00:00:00") {
+		t.FailNow()
 	}
 }
