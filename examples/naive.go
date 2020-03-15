6c5c9ea9a9865dcb2590c34ef65de71412eb3a01
sonirico <marsanben92@gmail.com>
2020-03-15 21:24:40 +0100

    doc(examples): add naive examples

---
+++ examples/naive.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sonirico/datetoken.go"
+)
+
+func main() {
+	tokens := []string{
+		"now/s",
+		"now/m",
+		"now/h",
+		"now/d",
+		"now/w",
+		"now/bw",
+		"now/M",
+		"now/Y",
+	}
+	fmt.Println("Snap to start of units")
+	for _, token := range tokens {
+		date, _ := datetoken.EvalNaive(token)
+		fmt.Printf("\n{token: %s, time: %s}", token, date)
+	}
+	fmt.Printf("\n")
+	fmt.Println("Snap to end of units")
+	tokens = []string{
+		"now@s",
+		"now@m",
+		"now@h",
+		"now@d",
+		"now@w",
+		"now@bw",
+		"now@M",
+		"now@Y",
+	}
+	for _, token := range tokens {
+		date, _ := datetoken.EvalNaive(token)
+		fmt.Printf("\n{token: %s, time: %s}", token, date)
+	}
+	fmt.Printf("\n")
+}
